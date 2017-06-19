6bd9b3825b63cad5db2e849db36c9b7972524b34
Jay Zeschin <jay.zeschin@modeset.com>
2017-06-19 17:23:36 -0600

    Allow sslmode to be specified in REDSHIFT_URL
    Default it to `require` if it's not supplied

--- lib/redshift/client/configuration.rb
+++ lib/redshift/client/configuration.rb
@@ -1,10 +1,13 @@
 require 'uri'
+require 'cgi'
 require 'active_support/core_ext/hash/reverse_merge'
 
 module Redshift
   module Client
     class Configuration
       attr_reader :host, :port, :user, :password, :dbname
+
+      DEFAULT_SSL_MODE = 'require'.freeze
 
       class << self
         def resolve(config = {})
@@ -15,31 +18,42 @@ module Redshift
             config[:port],
             config[:user],
             config[:password],
-            config[:dbname]
+            config[:dbname],
+            config[:sslmode]
           )
         end
 
         private
+
         def parse_redshift_url
-          uri = URI.parse(ENV["REDSHIFT_URL"])
+          uri = URI.parse(ENV['REDSHIFT_URL'])
           {
             host: uri.host,
             port: uri.port,
             user: uri.user,
             password: uri.password,
-            dbname: uri.path[1..-1]
+            dbname: uri.path[1..-1],
+            sslmode: sslmode(uri)
           }
         rescue
           {}
         end
+
+        def sslmode(uri)
+          if uri.query
+            param = CGI.parse(uri.query)['sslmode']
+            param && param[0]
+          end
+        end
       end
 
-      def initialize(host, port, user, password, dbname)
+      def initialize(host, port, user, password, dbname, sslmode)
         @host = host
         @port = port || 5439
         @user = user
         @password = password
         @dbname = dbname
+        @sslmode = sslmode || DEFAULT_SSL_MODE
       end
 
       def params
@@ -48,7 +62,8 @@ module Redshift
           port: @port,
           user: @user,
           password: @password,
-          dbname: @dbname
+          dbname: @dbname,
+          sslmode: @sslmode
         }
       end
     end
