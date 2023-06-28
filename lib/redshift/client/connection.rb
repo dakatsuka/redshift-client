9ea56e42e0aca072524812e011e45c999f1b6cd8
Balazs Andre <andre.balazs@kaligo.com>
2023-06-28 11:29:42 +0200

    Cast timezones properly to application timezone

--- lib/redshift/client/connection.rb
+++ lib/redshift/client/connection.rb
@@ -7,7 +7,7 @@ module Redshift
 
       def initialize(configuration)
         @original = PG.connect(configuration.params)
-        @original.type_map_for_results = PG::BasicTypeMapForResults.new(@original)
+        @original.type_map_for_results = extended_type_map_for(@original)
       end
 
       def method_missing(method_name, *args, &block)
@@ -20,6 +20,22 @@ module Redshift
 
       def respond_to_missing?(method_name, include_private = false)
         @original.respond_to?(method_name, include_private) || super
+      end
+
+      private
+
+      def extended_type_map_for(connection)
+        PG::BasicTypeMapForResults.new(connection, registry: registry)
+      end
+
+      def registry
+        # PG::BasicTypeMap assumes DB is in the same tz as application
+        # This extension supports Database which stores data in UTC if the column type is timezone(without zone)
+        timestamp_tz_parser_klass = PG::TextDecoder::TimestampUtcToLocal
+        registry = PG::BasicTypeRegistry.new
+        registry.register_default_types
+        registry.register_type(0, 'timestamp', PG::TextEncoder::TimestampWithoutTimeZone, timestamp_tz_parser_klass)
+        registry
       end
 
     end
--- spec/redshift/client/connection_spec.rb
+++ spec/redshift/client/connection_spec.rb
@@ -3,10 +3,19 @@ require 'spec_helper'
 describe Redshift::Client::Connection do
   let(:configuration) { Redshift::Client::Configuration.resolve }
   let(:connection) { double('connection', 'type_map_for_results=' => nil, 'exec' => nil) }
+  let(:registry) { double('registry') }
 
   describe "#initialize" do
     before do
       allow(PG).to receive(:connect)
+      allow(PG::BasicTypeRegistry).to receive(:new).and_return(registry)
+      allow(registry)
+        .to receive(:register_default_types)
+      allow(registry)
+        .to receive(:coders_for)
+      allow(registry)
+        .to receive(:register_type)
+        .with(0, 'timestamp', PG::TextEncoder::TimestampWithoutTimeZone, PG::TextDecoder::TimestampUtcToLocal)
     end
 
     it "calls PG#connect" do
@@ -26,7 +35,7 @@ describe Redshift::Client::Connection do
         .and_return(connection)
       expect(PG::BasicTypeMapForResults)
         .to receive(:new)
-        .with(connection)
+        .with(connection, registry: registry)
         .once
       Redshift::Client::Connection.new(configuration)
     end
